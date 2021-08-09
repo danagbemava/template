295b7491b3b0b55ab872435755c1f5e56c258fa5
Daniel Agbemava <danagbemava@gmail.com>
2021-08-09 21:42:46 +0000

    fix: incomplete baseChangeNotifier

--- lib/shared/viewmodels/base_change_notifier.dart
+++ lib/shared/viewmodels/base_change_notifier.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:template/shared/utils/logger_mixin.dart';
 
-abstract class BaseChangeNotifier<T> with LoggerMixin<T> {
+abstract class BaseChangeNotifier<T> extends ChangeNotifier
+    with LoggerMixin<T> {
   void setState(VoidCallback? fn) {
     if (fn != null) {
       fn();
     }
+    notifyListeners();
   }
 }
